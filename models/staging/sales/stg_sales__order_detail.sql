ebd0ccedb053681a3515f48af4281c4a29050d44
Vagner Constantini <vagnerconstantini@gmail.com>
2025-08-26 01:44:23 +0000

    add stg_sales__order_detail + tests/docs

---
+++ models/staging/sales/stg_sales__order_detail.sql
@@ -0,0 +1,25 @@
+with
+    source as (
+        select
+            SalesOrderID
+            , SalesOrderDetailID
+            , ProductID
+            , OrderQty
+            , UnitPrice
+            , UnitPriceDiscount
+        from {{ source('raw_adventure_works', 'salesorderdetail') }}
+    )
+
+    , renamed as (
+        select
+            cast(SalesOrderID as number)             as sales_order_id
+            , cast(SalesOrderDetailID as number)     as sales_order_detail_id
+            , cast(ProductID as number)              as product_id
+            , cast(OrderQty as number)               as order_qty
+            , cast(UnitPrice as number(18,2))        as unit_price
+            , cast(UnitPriceDiscount as number(9,4)) as unit_price_discount
+        from source
+    )
+
+select *
+from renamed
--- models/staging/sales/stg_sales__order_header.sql
+++ models/staging/sales/stg_sales__order_header.sql
@@ -1,21 +1,33 @@
 with
     source as (
-        select *
+        select
+            SalesOrderID
+            , OrderDate
+            , Status
+            , CustomerID
+            , ShipToAddressID
+            , CreditCardID
+            , SubTotal
+            , TaxAmt
+            , Freight
+            , TotalDue
         from {{ source('raw_adventure_works', 'salesorderheader') }}
     )
-  , renamed as (
+
+    , renamed as (
         select
-            cast(SalesOrderID as number)    as sales_order_id
-          , cast(OrderDate as date)         as order_date
-          , cast(Status as number)          as status_code
-          , cast(CustomerID as number)      as customer_id
-          , cast(ShipToAddressID as number) as ship_to_address_id
-          , cast(CreditCardID as number)    as credit_card_id
-          , cast(SubTotal as number(18,2))  as subtotal_amount
-          , cast(TaxAmt as number(18,2))    as tax_amount
-          , cast(Freight as number(18,2))   as freight_amount
-          , cast(TotalDue as number(18,2))  as total_due_amount
+            cast(SalesOrderID as number)      as sales_order_id
+            , cast(OrderDate as date)         as order_date
+            , cast(Status as number)          as status_code
+            , cast(CustomerID as number)      as customer_id
+            , cast(ShipToAddressID as number) as ship_to_address_id
+            , cast(CreditCardID as number)    as credit_card_id
+            , cast(SubTotal as number(18,2))  as subtotal_amount
+            , cast(TaxAmt as number(18,2))    as tax_amount
+            , cast(Freight as number(18,2))   as freight_amount
+            , cast(TotalDue as number(18,2))  as total_due_amount
         from source
     )
+
 select *
 from renamed
