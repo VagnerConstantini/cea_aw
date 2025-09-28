ad4c32670d52814e7de2cfd0949780c0ad5cbfae
Vagner Constantini <vagnerconstantini@gmail.com>
2025-09-28 12:48:12 +0000

    fix: remove invalid unique test from dim_date.year_month_sort

--- models/marts/sales/dim_date.sql
+++ models/marts/sales/dim_date.sql
@@ -28,26 +28,3 @@ select
     cast(to_char(date_day, 'YYYY-MM') as string) as year_month,       -- "2025-09"
     cast(to_char(date_day, 'YYYYMM')  as number) as year_month_sort   -- 202509
 from spine
-
-/*
-{{ config(materialized='table') }}
-
-with spine as (
-
-    {{ dbt_utils.date_spine(
-        datepart="day"
-        , start_date="cast('2000-01-01' as date)"
-        , end_date="cast('2030-12-31' as date)"
-    ) }}
-
-)
-
-select
-    {{ dbt_utils.generate_surrogate_key(["'DT0'", "date_day"]) }} as date_key
-    , cast(date_day                     as date)                  as date_day
-    , cast(extract(year  from date_day) as number)                as year
-    , cast(extract(month from date_day) as number)                as month_number
-    , cast(to_char(date_day, 'Mon')     as string)                as month_name   -- "Jan", "Feb", ...
-    , cast(to_char(date_day, 'YYYY-MM') as string)                as year_month   -- "2025-09"
-from spine
-*/
